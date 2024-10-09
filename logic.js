32afe41160680535460dcc3d791f73d126c7762e
Grant <grantley.kuo@gmail.com>
2024-10-08 21:57:29 -0700

    Update logic.js

--- logic.js
+++ logic.js
@@ -23,7 +23,7 @@ async function assignOpenWeatherJSON(position) {
     console.log("Lat : " + lat + " Lon: " + lng);
     
 
-    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${'Your API key'}`)
+    await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${'3192dc335da17718eeb8d5c1159826f5'}`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -40,6 +40,9 @@ async function displayWindData()
 {
     console.log("wind angle: " + windAngle);
     console.log("wind speed: " + windSpeed);
+    
+
+
 }
 
 
@@ -52,131 +55,3 @@ getLocationWeather();
 
 
 
-
-// fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=Your API key`, {
-//   method: 'POST',
-//   headers: {
-//     'Accept': 'application/json',
-//     'Content-Type': 'application/json'
-//   },
-// }).then((response) => response.json()).then((jsonData) => {
-//     console.log(jsonData);
-//     returnedLongANDLatANDAccuracyJSON = jsonData;
-    
-//     lat = Number(returnedLongANDLatANDAccuracyJSON.location.lat.toFixed(4));
-//     lng = Number(returnedLongANDLatANDAccuracyJSON.location.lng.toFixed(4));
-//     console.log("lat " + lat);
-//     console.log("lng " + lng);
-    
-    
-
-//     let params = 'windDirection';
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?lat={180}&lon={180}&appid={Your API key}`, {
-//     // headers: {
-//     //     'Authorization': 'Your API key'
-//     // }
-//         }).then((response) => response.json()).then((jsonData) => {
-        
-//             console.log(jsonData);
-
-             
-//             // assign current time in UTC to match API time config
-//             const date = new Date();
-//             const hour = date.getUTCHours();
-//             console.log(hour);
-
-//             //access the hours array and find the wind angle for the current hour
-//             // 0° indicates wind COMING from north
-//             const hours = jsonData.hours;
-//             // const hours = hoursArray.hours;
-//             const windAngleNOAA = hours[hour].windDirection.noaa;
-//             const windAngleSG = hours[hour].windDirection.sg;
-//             const windAngle = ((windAngleNOAA - windAngleSG)/2) + windAngleSG;
-
-        
-//             console.log("wind angle is" + windAngle);
-
-            let canv = document.getElementById("windDirectionArrow");
-            let ctx = canv.getContext("2d");
-            ctx.translate(150, 150);
-
-            // let windDirection;
-            // if((windAngle >= 338 && windAngle <= 360) || (windAngle >= 0 && windAngle <= 22))
-            // {
-            //     windDirection = "South";
-            //     ctx.rotate((180) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 23 && windAngle <= 67)
-            // {
-            //     windDirection = "South West";
-            //     ctx.rotate((180 + 45) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 68 && windAngle <= 112)
-            // {
-            //     windDirection = "West";
-            //     ctx.rotate((270) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 113 && windAngle <= 157)
-            // {
-            //     windDirection = "North West";
-            //     ctx.rotate((315) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 158 && windAngle <= 202)
-            // {
-            //     windDirection = "North";
-            // }
-            // else if(windAngle >= 203 && windAngle <= 247)
-            // {
-            //     windDirection = "North East";
-            //     ctx.rotate((45) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 248 && windAngle <= 292)
-            // {
-            //     windDirection = "East";
-            //     ctx.rotate((90) * Math.PI / 180);
-            // }
-            // else if(windAngle >= 293 && windAngle <= 337)
-            // {
-            //     windDirection = "South East";
-            //     ctx.rotate((90 + 45) * Math.PI / 180);
-            // }
-
-            // document.getElementById('windDirectionAngle').innerHTML = "&emsp;The current wind direction is: " + windDirection;
-            
-            // var imageARROW = new Image();
-            // imageARROW.src = "arrow.png";
-        
-            // imageARROW.onload = function() {
-            //     ctx.drawImage(imageARROW, -150, -150, 300, 300);
-            // };
-
-
-            // //fetch wind speed
-            // params = 'windSpeed';
-            // fetch(`https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`, {
-            // headers: {
-            //     'Authorization': 'Your API key'
-            // }
-            //     }).then((response) => response.json()).then((jsonData) => {
-            //         console.log("windSpeed Object:");
-            //         console.log(jsonData);
-
-            //         const hours = jsonData.hours;
-
-            //         const windSpeedNOAA = hours[hour].windSpeed.noaa;
-            //         const windSpeedSG = hours[hour].windSpeed.sg;
-
-            //         //the API documentation states that wind speed is returned in meters per second
-            //         //i think this is a typo?
-            //         //I have double checked Long and Lat coordinates and checked wind speeds from
-            //         //https://www.windfinder.com/forecast/burnaby_british_columbia_canada 
-            //         //lets assume there is a type and indeed the wind speed is returned in KMPH
-            //         const windSpeedInKMPH = Math.round(((windSpeedNOAA - windSpeedSG)/2) + windSpeedSG);
-                    
-                    
-            //         console.log("wind speed is: " + windSpeedInKMPH);
-            //         document.getElementById('windSpeed').innerHTML = "&emsp;&emsp;The current wind Speed is: " + windSpeedInKMPH + " km/h";
-            // });
-//     });
-
-// });
